15b4409ce51b92e6b5fd7beaf90913161f018416
Eric Snell <eric_snell21@yahoo.com>
2017-07-14 11:45:39 -0400

    live video feed

--- js/script.js
+++ js/script.js
@@ -9,10 +9,18 @@
   // Access users webcam
   function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      // returns a Promise...
       .then(localMediaStream => {
-        console.log(localMediaStream);
+        // convert media stream obj into url and set as video src attribute
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
       })
+      .catch(err => {
+        console.error(`OH NO!!!`, err);
+      });
   }
+
+  getVideo();
 
 
   
